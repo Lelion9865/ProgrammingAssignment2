e94fe36c6df4bd483c503147d5c0636a7826a9f0
Lionel <LION0019@e.ntu.edu.sg>
2020-07-24 22:48:53 +0800

    The idea of this question is the same as cacheMean.R. Change the mean function to solve to find the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The idea of this question is the same as cacheMean.R
+## Simply change the function from mean to solve
+## Some examples to test the function:
+## x = matrix(c(4,0,0,1,0,0,1,0,0,2,2,0,0,0,0,1), 4, 4)
+## x = matrix(c(1,0,5,2,1,6,3,4,0))
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
